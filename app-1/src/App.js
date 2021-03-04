a4905883eb958f6b2d823df8aae712209e4e9aa7
Mustafa Khan <mustafakhan98@gmail.com>
2021-03-04 06:08:50 +0300

    app1 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state={
+      message: ''
+    }
+  }
+
+  printDom(val){
+    this.setState({message: val})
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='show'>
+      <input className = 'to-dom' onChange={(event) => this.printDom(event.target.value)}></input>
+      <p>{(this.state.message)}</p>
     </div>
-  );
+  )
+  }
 }
 
 export default App;
